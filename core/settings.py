c3dd03e5ea79c7fdb7ae3d707e9f968d56527abb
Atik Abdullah <118540668+knightphoenix101@users.noreply.github.com>
2025-05-05 06:17:36 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p5!lhdmlf))@qz(679+go)^)_3ni_@l-ei-gm)3%k(d5)%gx19'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movie-application-l056.onrender.com']
 
 
 # Application definition
